ef4f9901b9cd39d1b38dbf1d2b4cec8ac4639629
maheesh123 <139621164+maheesh123@users.noreply.github.com>
2024-12-23 11:57:44 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "wmyYlV4afhTc9s0hvdFJww==cGyBzH7PzcYOjGfE";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+}; 
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes"; 
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+     jokeEl.innerText= data[0].joke
+      btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    //  console.log(data)
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
